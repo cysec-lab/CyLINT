e77c39256843796f65f6e4ce142e17734c3a6069
xryuseix <ryusei143.shootingstar@gmail.com>
2023-05-18 03:57:23 +0900

    refactor: tailwindをより多く使うようにした

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,15 +1,15 @@
 import styles from "./LoadingScreen.module.css";
 
 export const LoadingScreen = () => (
-  <div className={`${styles.container} ${styles.normal}`}>
-    <div className={styles.spinner} />
-    <div className={styles.text}>Loading...</div>
+  <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className={`${styles.spinner} mb-4`} />
+    <div className="text-lg">{`Loading...`}</div>
   </div>
 );
 
 export const LoadingScreenClear = () => (
-  <div className={`${styles.container} ${styles.clear}`}>
-    <div className={styles.spinner} />
-    <div className={styles.text}>Loading...</div>
+  <div className="flex items-center justify-center h-screen bg-gray-100 absolute top-0 left-0 right-0 bottom-0">
+    <div className={`${styles.spinner} mr-4`} />
+    <div className="text-lg font-bold">{`Loading...`}</div>
   </div>
 );
