d929f55063f16d5f0292723c1ae70544018a8d09
xryuseix <ryusei143.shootingstar@gmail.com>
2023-05-18 03:57:23 +0900

    refactor: tailwindをより多く使うようにした

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -1,12 +1,14 @@
 import { useAuth } from "@/contexts/AuthContext";
-import styles from "./LoginScreen.module.css";
 
 export const LoginScreen = () => {
   const { signInWithGoogle } = useAuth();
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Please Login</h1>
-      <button className={styles.button} onClick={signInWithGoogle}>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl mb-4">Please Login</h1>
+      <button
+        className="px-8 py-4 text-lg font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 transform transition-all duration-200 ease-in-out"
+        onClick={signInWithGoogle}
+      >
         Google Login (only Cysec-lab Students)
       </button>
     </div>
