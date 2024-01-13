5953f9fb1c92bc2f4f7ea123bb33fb3b7ae591ca
xryuseix <ryusei143.shootingstar@gmail.com>
2024-01-13 16:28:11 +0900

    fix: submit button style

--- src/components/LaTeXForm.tsx
+++ src/components/LaTeXForm.tsx
@@ -72,7 +72,7 @@ export const LaTeXForm = () => {
       <div className="flex flex-col justify-center items-center">
         <button
           id="submit"
-          className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md w-1/2"
+          className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md w-1/2 shadow-lg hover:shadow-md"
           onClick={handleClick}
           disabled={loading}
         >
